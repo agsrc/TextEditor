c12b8d79479077cb3fa777d78cc073f8d415fe1d
agsrc <akshay0712@gwu.edu>
2019-08-07 17:19:45 -0400

    efficient method

--- MOOCTextEditor/src/document/EfficientDocument.java
+++ MOOCTextEditor/src/document/EfficientDocument.java
@@ -51,9 +51,20 @@ public class EfficientDocument extends Document {
 		// MAKE SURE YOU UNDERSTAND THIS LINE BEFORE YOU CODE THE REST
 		// OF THIS METHOD.
 		List<String> tokens = getTokens("[!?.]+|[a-zA-Z]+");
-		
-		// TODO: Finish this method.  Remember the countSyllables method from 
-		// Document.  That will come in handy here.  isWord defined above will also help.
+		numWords = 0;
+		numSentences = 0;
+		numSyllables = 0;
+		for (int i=0;i<tokens.size();i++) {
+			if (isWord(tokens.get(i))) {
+				numWords += 1;
+				numSyllables += countSyllables(tokens.get(i));
+				if(i == tokens.size() -1) {
+					numSentences += 1;
+				}
+			} else {
+				numSentences += 1;
+			}
+		}
 	}
 
 	
@@ -73,7 +84,7 @@ public class EfficientDocument extends Document {
 	@Override
 	public int getNumSentences() {
 		//TODO: write this method.  Hint: It's simple
-		return 0;
+		return numSentences;
 	}
 
 	
@@ -93,8 +104,8 @@ public class EfficientDocument extends Document {
 	 */
 	@Override
 	public int getNumWords() {
-		//TODO: write this method.  Hint: It's simple
-	    return 0;
+		
+	    return numWords;
 	}
 
 
@@ -115,8 +126,8 @@ public class EfficientDocument extends Document {
 	 */
 	@Override
 	public int getNumSyllables() {
-        //TODO: write this method.  Hint: It's simple
-        return 0;
+        
+        return numSyllables;
 	}
 	
 	// Can be used for testing

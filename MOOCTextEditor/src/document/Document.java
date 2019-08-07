65af4f50e1d8feb4da249d1f44c7084bdb20dcce
agsrc <akshay0712@gwu.edu>
2019-08-07 15:03:59 -0400

    Implement getNumSentences, getNumWords,  getNumSyllables and fleschScore methods

--- MOOCTextEditor/src/document/Document.java
+++ MOOCTextEditor/src/document/Document.java
@@ -64,10 +64,27 @@ public abstract class Document {
 	 */
 	protected int countSyllables(String word)
 	{
-		// TODO: Implement this method so that you can call it from the 
-	    // getNumSyllables method in BasicDocument (module 2) and 
-	    // EfficientDocument (module 3).
-	    return 0;
+	    //System.out.print("Counting syllables in " + word + "...");
+		int numSyllables = 0;
+		boolean newSyllable = true;
+		String vowels = "aeiouy";
+		char[] cArray = word.toCharArray();
+		for (int i = 0; i < cArray.length; i++)
+		{
+		    if (i == cArray.length-1 && Character.toLowerCase(cArray[i]) == 'e' 
+		    		&& newSyllable && numSyllables > 0) {
+                numSyllables--;
+            }
+		    if (newSyllable && vowels.indexOf(Character.toLowerCase(cArray[i])) >= 0) {
+				newSyllable = false;
+				numSyllables++;
+			}
+			else if (vowels.indexOf(Character.toLowerCase(cArray[i])) < 0) {
+				newSyllable = true;
+			}
+		}
+		//System.out.println( "found " + numSyllables);
+		return numSyllables;
 	}
 	
 	/** A method for testing
@@ -130,9 +147,11 @@ public abstract class Document {
 	/** return the Flesch readability score of this document */
 	public double getFleschScore()
 	{
-	    // TODO: You will play with this method in week 1, and 
-		// then implement it in week 2
-	    return text.length();
+	    double fleschScore;
+	    BasicDocument bdObject = new BasicDocument(text);
+	    fleschScore = 206.835 -1.015*(bdObject.getNumWords()/bdObject.getNumSentences())-84.6*(bdObject.getNumSyllables()/bdObject.getNumWords());
+		System.out.println("fleschScore:"+fleschScore);
+		return fleschScore;
 	}
 	
 	
